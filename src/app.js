dec91c19461995867c1976687041169528479d60
Olya* <go.olya10@gmail.com>
2023-06-30 11:06:48 +0100

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
-function displayTemperarure(response) {
-  console.log(response.data);
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "64c8b0951401b62532bda560ec6ec228";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperarure);
+axios.get(apiUrl).then(displayTemperature);
