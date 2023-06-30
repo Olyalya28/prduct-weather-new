4a90f53f2eef852033914c23ddc79453da5e236d
Olya* <go.olya10@gmail.com>
2023-06-30 10:09:14 +0100

    added change apiurl

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ function displayTemperarure(response) {
 }
 
 let apiKey = "64c8b0951401b62532bda560ec6ec228";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid={apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperarure);
